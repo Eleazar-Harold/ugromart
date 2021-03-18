93c1e2359e10da7de2509c7c6afcf52128db388e
eleazar harold <haroldyewa@gmail.com>
2021-03-18 23:26:31 +0300

    updated order-service.

--- order-service/app/api/order.py
+++ order-service/app/api/order.py
@@ -7,21 +7,12 @@ from app.api.models import (
     OrderUpdate,
 )
 from app.api import manager
-from app.api.service import is_cast_present
 
 ordersapi = APIRouter()
 
 
 @ordersapi.post("/", response_model=OrderOut, status_code=201)
 async def create_orders(payload: OrderIn):
-    # to be revisited
-    for cast_id in payload.casts_id:
-        if not is_cast_present(cast_id):
-            raise HTTPException(
-                status_code=404,
-                detail=f"Cast with given id:{cast_id} not found",
-            )
-
     order_id = await manager.create(payload)
     response = {"id": order_id, **payload.dict()}
     return response
@@ -53,15 +44,6 @@ async def update_order(id: int, payload: OrderUpdate):
         )
 
     update_data = payload.dict(exclude_unset=True)
-
-    # to be revisited
-    if "casts_id" in update_data:
-        for cast_id in payload.casts_id:
-            if not is_cast_present(cast_id):
-                raise HTTPException(
-                    status_code=404,
-                    detail=f"Cast with given id:{cast_id} not found",
-                )
 
     order_in_db = OrderIn(**order)
     updated_order = order_in_db.copy(update=update_data)
